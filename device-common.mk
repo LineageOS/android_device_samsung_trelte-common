e0a347d89fdbc2d43f3e375f80c6c2c87163fd6b
ljzyal <ljzyal@gmail.com>
2016-02-17 22:23:55 +0800

    exynos5: Support Fingerprint Hardware
    Change-Id: I41c1f9bfa3c6ad11ce9726c467b977384471a38e

--- device-common.mk
+++ device-common.mk
@@ -25,6 +25,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
@@ -176,6 +177,16 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     Snap
+
+###########################################################
+### FINGERPRINT
+###########################################################
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    fingerprintd \
+    fingerprint.exynos5 \
+    ValidityService
 
 ###########################################################
 ### TOUCHSCREEN
