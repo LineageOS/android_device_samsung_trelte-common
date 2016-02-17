4de178cf24c282f9cc95162c37525fbba5d695ee
ljzyal <ljzyal@gmail.com>
2016-02-17 22:23:55 +0800

    exynos5: Support Fingerprint Hardware
    Change-Id: I41c1f9bfa3c6ad11ce9726c467b977384471a38e

--- device-common.mk
+++ device-common.mk
@@ -20,6 +20,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
@@ -57,6 +58,12 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libstlport
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    fingerprintd \
+    fingerprint.exynos5 \
+    ValidityService
 
 # Hardware/Samsung/AdvancedDisplay (MDNIE)
 PRODUCT_PACKAGES += \
